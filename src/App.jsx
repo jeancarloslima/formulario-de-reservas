bd9258d3cce85b016ba71bf14e12b0dec9f79daa
Jean Lima <jcoliveiralima19@gmail.com>
2026-04-29 12:24:51 -0300

    Instalando bibliotecas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { Button } from "@/components/ui/button";
 
 function App() {
 
   return (
     <>
+    <h1 className='text-xl text-blue-500'>Teste</h1>
+    <Button>Clique em mim!</Button>
     </>
   )
 }
