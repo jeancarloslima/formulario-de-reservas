01cb9d2653e1b4ffebd6b8b5ebb042fa0437c32d
Jean Lima <jcoliveiralima19@gmail.com>
2026-04-30 13:49:03 -0300

    Desativando botões nos extremos

--- src/components/assets/ButtonsBar/index.jsx
+++ src/components/assets/ButtonsBar/index.jsx
@@ -2,13 +2,14 @@ import { Button } from "@/components/ui/button";
 import { useReservationStore } from "../../contexts/useReservationStore";
 
 export default function ButtonsBar() {
-    const nextScreen = useReservationStore((state) => state.nextScreen)
-    const previousScreen = useReservationStore((state) => state.previousScreen)
+    const currentScreen = useReservationStore((state) => state.currentScreen);
+    const nextScreen = useReservationStore((state) => state.nextScreen);
+    const previousScreen = useReservationStore((state) => state.previousScreen);
 
     return (
         <div className="w-full max-w-[450px] flex justify-between">
-            <Button className="w-[120px] bg-black text-white cursor-pointer hover:bg-gray-700" onClick={previousScreen}>PREVIOUS</Button>
-            <Button className="w-[120px] bg-blue-500 cursor-pointer hover:bg-blue-400" onClick={nextScreen}>NEXT</Button>
+            <Button className={`w-[120px] bg-black text-white cursor-pointer hover:bg-gray-700 ${currentScreen === 1 ? "pointer-events-none bg-gray-400" : ""}`} onClick={previousScreen}>PREVIOUS</Button>
+            <Button className={`w-[120px] bg-blue-500 cursor-pointer hover:bg-blue-400 ${currentScreen === 4 ? "pointer-events-none bg-gray-400" : ""}`} onClick={nextScreen}>NEXT</Button>
         </div>
     )
 }
