2c94781af2e2afd63018fe42d7d031af41046973
Jean Lima <jcoliveiralima19@gmail.com>
2026-04-30 13:22:52 -0300

    Criando usereservationstore

--- src/components/assets/ButtonsBar/index.jsx
+++ src/components/assets/ButtonsBar/index.jsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function ButtonsBar() {
     return (
         <div className="w-full max-w-[450px] flex justify-between">
-            <Button className="w-[120px] bg-black text-white cursor-pointer">PREVIOUS</Button>
-            <Button className="w-[120px] bg-blue-500 cursor-pointer">NEXT</Button>
+            <Button className="w-[120px] bg-black text-white cursor-pointer hover:bg-gray-700">PREVIOUS</Button>
+            <Button className="w-[120px] bg-blue-500 cursor-pointer hover:bg-blue-400">NEXT</Button>
         </div>
     )
 }
---
+++ src/components/contexts/useReservationStore/index.jsx
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+
+export const useReservationStore = create((set) => ({
+  currentScreen: 1,
+
+  reservationData: {
+    checkIn: null,
+    checkOut: null,
+    guests: 1,
+    roomId: null,
+    name: "",
+    email: "",
+  },
+
+  nextScreen: () => {
+    set((state) => ({ currentScreen: state.currentScreen + 1 }));
+  },
+
+  previousScreen: () => {
+    set((state) => ({ currentScreen: state.currentScreen - 1 }));
+  },
+
+  updateData: (newData) => {
+    set((state) => ({
+      reservationData: { ...state.reservationData, newData },
+    }));
+  },
+}));
