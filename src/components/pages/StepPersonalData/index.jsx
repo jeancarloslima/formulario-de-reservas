66a51396c1af773159360404268216f363a0f62e
Jean Lima <jcoliveiralima19@gmail.com>
2026-05-05 14:35:40 -0300

    Adicionando tela de dados pessoais

--- src/components/pages/StepPersonalData/index.jsx
+++ src/components/pages/StepPersonalData/index.jsx
@@ -1,3 +1,39 @@
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
 export default function StepPersonalData() {
-    return <h1>Personal Data</h1>
+    return (
+        <div>
+            <form>
+                <FieldGroup>
+                    <FieldSet>
+                        <FieldLegend>Personal Data</FieldLegend>
+                        <FieldDescription>We need some personal data to reserve the room.</FieldDescription>
+                        <FieldGroup>
+                            <Field>
+                                <FieldLabel htmlFor="name">
+                                    Name
+                                </FieldLabel>
+                                <Input id="name" placeholder="John Doe" required />
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="email">
+                                    Email
+                                </FieldLabel>
+                                <Input id="email" placeholder="johndoe@gmail.com" required />
+                            </Field>
+                        </FieldGroup>
+                    </FieldSet>
+                </FieldGroup>
+            </form>
+        </div>
+    )
 }
