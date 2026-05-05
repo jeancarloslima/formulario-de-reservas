a0c8ae79bb3508453c96a449b28809c94ce46ce3
Jean Lima <jcoliveiralima19@gmail.com>
2026-05-05 14:20:04 -0300

    Adicionando tela de confirmar dados

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,10 @@ function App() {
 
   return (
     <div className="min-h-screen w-screen p-4 flex justify-center items-center bg-gray-300">
-      <div className="bg-white rounded shadow-md w-full max-w-[600px] h-[600px] px-4 py-12 mx-auto flex flex-col items-center justify-between gap-8">
+      <div className="bg-white rounded shadow-md w-full max-w-[600px] h-[600px] px-8 py-12 mx-auto flex flex-col items-center justify-between gap-8">
         <ProgressBar />
 
-        <div>{renderStep()}</div>
+        <div className="w-full max-w-[400px]">{renderStep()}</div>
 
         <ButtonsBar />
       </div>
--- src/components/pages/StepConfirmation/index.jsx
+++ src/components/pages/StepConfirmation/index.jsx
@@ -1,3 +1,35 @@
+import { useReservationStore } from "../../contexts/useReservationStore";
+
 export default function StepConfirmation() {
-    return <h1>Confirmation</h1>
+  const reservationData = useReservationStore((state) => state.reservationData);
+
+  return (
+    <div className="w-full flex flex-col gap-6">
+      <h2 className="font-bold text-center text-green-500">Confirm the data</h2>
+      <div className="flex justify-between">
+        <h2 className="font-bold">Check-in:</h2>
+        <p>{reservationData.checkIn}</p>
+      </div>
+
+      <div className="flex justify-between">
+        <h2 className="font-bold">Check-out:</h2>
+        <p>{reservationData.checkOut}</p>
+      </div>
+
+      <div className="flex justify-between">
+        <h2 className="font-bold">Guests:</h2>
+        <p>{reservationData.guests}</p>
+      </div>
+
+      <div className="flex justify-between">
+        <h2 className="font-bold">Name:</h2>
+        <p>{reservationData.name}</p>
+      </div>
+
+      <div className="flex justify-between">
+        <h2 className="font-bold">Email:</h2>
+        <p>{reservationData.email}</p>
+      </div>
+    </div>
+  );
 }
