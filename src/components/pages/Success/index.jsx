a94f8baafc4dfbd10cfa7110bc261bd1ee4d14fb
Jean Lima <jcoliveiralima19@gmail.com>
2026-05-07 12:35:48 -0300

    Alterando design do success

--- src/components/pages/Success/index.jsx
+++ src/components/pages/Success/index.jsx
@@ -1,8 +1,11 @@
+import { Trophy } from "lucide-react";
+
 export default function Success() {
     return (
-        <div className="bg-white p-4 rounded">
-            <h2 className="text-xl font-bold text-center text-green-500 mb-2">Your reservation is done!</h2>
-            <p className="text-gray-500 w-[200px] mx-auto text-center">You will recieve an email with the confirmation and aditional data.</p>
+        <div className="w-full max-w-[450px] bg-gray-200 p-4 rounded text-center">
+            <Trophy size={60} color="#22c55e" className="mx-auto my-8" />
+            <h2 className="text-xl font-bold text-green-500 mb-2">Your reservation is done!</h2>
+            <p className="text-gray-500 w-[200px] mx-auto mb-4">You will recieve an email with the confirmation and aditional data. <br /><br /> <span className="text-green-500 font-bold">Enjoy your stay!</span></p>
         </div>
     )
 }
